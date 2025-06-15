ee58b9937959ad0f77fc45d34b45973d0725505d
Dalton Ousley <daltonjousley@gmail.com>
2025-06-14 19:18:23 -0600

    fix: chrome object for API

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 
 tabBtn.addEventListener("click", function () {
-  createFetchableDevEnvironment.tabs.query(
-    { active: true, currentWindow: true },
-    function (tabs) {
-      myLeads.push(tabs[0].url);
-      localStorage.setItem("myLeads", JSON.stringify(myLeads));
-      render(myLeads);
-    }
-  );
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
 deleteBtn.addEventListener("dblclick", function () {
